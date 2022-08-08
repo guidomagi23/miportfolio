7b3bb64f3b6b42c6cc839ea06a24f6602b6aa376
guidomagi23 <guidomagi23@gmail.com>
2022-08-08 19:44:59 -0300

    actualizacion cv y about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,11 +7,10 @@ const About = () => {
     <div className='about-countainer'>
       <div className='about-desc'>
         <h3>Acerca de mi</h3>
-        <p>Me encuentro desarrollando mi proyecto final con React.JS y PHP
-          para finalizar mis estudios como Desarrollador de Software. Desde el
-          2013 formo parte del mundo de la programación mediante carreras de grados,
-          tecnicaturas, cursos y trabajos. Con ganas de seguir desarrollándome como
-          profesional y dispuesto a aprender.</p>
+        <p>Desde el 2013 formo parte del mundo IT mediante una carrera de grado, 
+          una tecnicatura, diversos cursos y trabajos. Mi último proyecto lo realicé con React y PHP 
+          y fue con el cual me recibí. Me gusta aprender y enseñar y estoy dispuesto a perfeccionarme 
+          para poder crecer íntegramente como profesional.</p>
       </div>
       <div className='about-img'>
         <img src={aboutImagen} alt="about" />
