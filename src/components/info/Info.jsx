e8108309ff4bc13115e011c5dbae5ee2f02e4c8f
guidomagi23 <guidomagi23@gmail.com>
2022-10-25 18:38:18 -0300

    actualizacion

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -6,6 +6,7 @@ const info = () => {
     <div className='info-container'>
       <div className='info'>
         <h1>No es solo un portfolio, es una curva de aprendizaje.</h1>
+        <h2>(Te invito a ver mi Github)</h2>
       </div>
 
     </div>
