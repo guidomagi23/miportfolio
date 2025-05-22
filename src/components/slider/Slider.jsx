0bcfaf0efaa9330710af50a9097ead10e15baf26
guidomagi23 <guidomagi23@gmail.com>
2025-05-22 19:44:25 -0300

    update style

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -7,11 +7,27 @@ import SlidesCipax from './SlidesCipax'
 
 const Slider = () => {
   return (
-    <div className='carousel-container' id="projects">
-      <div className='carousel-title'>
+    <div className="carousel-container" id="projects">
+      <div className="carousel-title cipax">
         <h2>Mis Proyectos</h2>
         <h3>Junto a Cipax: 2023-2024</h3>
-        <p>Trabajé en proyectos increíbles como parte de un equipo talentoso en <a href="https://cipax.dev/" title="Cipax.dev" target="_blank" rel="noreferrer">Cipax.dev</a>, desarrollando soluciones que priorizan la innovación y los resultados efectivos. Implementé aplicaciones web completas utilizando Laravel, WordPress, HTML, CSS, Bootstrap y Git, integrando APIs, optimizando bases de datos con Eloquent, y mejorando el rendimiento y la visibilidad en línea mediante estrategias de SEO, Google Analytics y metodologías ágiles como SCRUM. </p>
+        <p>
+          Trabajé en proyectos increíbles como parte de un equipo talentoso en{" "}
+          <a
+            href="https://cipax.dev/"
+            title="Cipax.dev"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Cipax.dev
+          </a>
+          , desarrollando soluciones que priorizan la innovación y los
+          resultados efectivos. Implementé aplicaciones web completas utilizando
+          Laravel, WordPress, HTML, CSS, Bootstrap y Git, integrando APIs,
+          optimizando bases de datos con Eloquent, y mejorando el rendimiento y
+          la visibilidad en línea mediante estrategias de SEO, Google Analytics
+          y metodologías ágiles como SCRUM.{" "}
+        </p>
       </div>
       <Carousel
         arrows
@@ -28,14 +44,21 @@ const Slider = () => {
             slidesPerPage: 1,
             arrows: true,
             itemWidth: 200,
-          }
+          },
         }}
-
       />
 
-      <div className='carousel-title'>
+      <div className="carousel-title">
         <h3>Personales: 2019-2022</h3>
-        <p>Durante mis estudios, desarrollé proyectos personales que me permitieron explorar y consolidar mis habilidades en diversas tecnologías. Creé aplicaciones y soluciones utilizando PhoneGap, JavaScript, HTML, CSS y PHP, integrando bases de datos MySQL. También experimenté con el desarrollo móvil nativo en Android Studio y construí interfaces dinámicas con React, combinando diseño y funcionalidades.</p>
+        <p>
+          Durante mis estudios, desarrollé proyectos personales que me
+          permitieron explorar y consolidar mis habilidades en diversas
+          tecnologías. Creé aplicaciones y soluciones utilizando PhoneGap,
+          JavaScript, HTML, CSS y PHP, integrando bases de datos MySQL. También
+          experimenté con el desarrollo móvil nativo en Android Studio y
+          construí interfaces dinámicas con React, combinando diseño y
+          funcionalidades.
+        </p>
       </div>
 
       <Carousel
@@ -53,12 +76,11 @@ const Slider = () => {
             slidesPerPage: 1,
             arrows: true,
             itemWidth: 200,
-          }
+          },
         }}
-
       />
     </div>
-  )
+  );
 }
 
 export default Slider
--- src/components/slider/SlidesCipax.jsx
+++ src/components/slider/SlidesCipax.jsx
@@ -11,96 +11,134 @@ import tastyImagen from "../../media/tasty.jpg";
 import latintechImagen from "../../media/latintech.jpg";
 import mezcalImagen from "../../media/mezcal.jpg";
 
-
 const slidesInfo = [
-    {
-        img: communityImagen,
-        alt: "Community", 
-        desc: "Community",
-        link: "https://community.phdcincubator.org/",
-        video: "https://community.phdcincubator.org/"
-    },
+  {
+    img: communityImagen,
+    alt: "Community",
+    desc: "Community",
+    letra: "C",
+    color: "#49aff7",
+    link: "https://community.phdcincubator.org/",
+    video: "https://community.phdcincubator.org/",
+  },
 
-    {
-        img: phdcImagen,
-        alt: "PHDC",
-        desc: "PHDC",
-        link: "https://phdcincubator.org/",
-        video: "https://phdcincubator.org/"
-    },
+  {
+    img: phdcImagen,
+    alt: "PHDC",
+    desc: "PHDC",
+    letra: "P",
+    color: "#baa182",
+    link: "https://phdcincubator.org/",
+    video: "https://phdcincubator.org/",
+  },
 
-    {
-        img: latintechImagen,
-        alt: "Latintech",
-        desc: "Latintech",
-        link: "https://latintechpgh.com/",
-        video: "https://latintechpgh.com/"
-    },
+  {
+    img: latintechImagen,
+    alt: "Latintech",
+    desc: "Latintech",
+    letra: "L",
+    color: "#0f888f",
+    link: "https://latintechpgh.com/",
+    video: "https://latintechpgh.com/",
+  },
 
-    {
-        img: DrfreezeImagen,
-        alt: "Drfreeze",
-        desc: "Drfreeze",
-        link: "https://drfreezellc.com/",
-        video: "https://drfreezellc.com/"
-    },
-    {
-        img: holapittImagen,
-        alt: "Holapittsburgh",
-        desc: "Holapittsburgh",
-        link: "https://holapittsburgh.com/",
-        video: "https://holapittsburgh.com/"
-    },
-    {
-        img: MLImagen,
-        alt: "Mlmultilingual",
-        desc: "Mlmultilingual",
-        link: "https://www.mlmultilingualservices.com/",
-        video: "https://www.mlmultilingualservices.com/"
-    },
+  {
+    img: DrfreezeImagen,
+    alt: "Drfreeze",
+    desc: "Drfreeze",
+    letra: "D",
+    color: "#7c7c7a",
+    link: "https://drfreezellc.com/",
+    video: "https://drfreezellc.com/",
+  },
+  {
+    img: holapittImagen,
+    alt: "Holapittsburgh",
+    desc: "Holapittsburgh",
+    letra: "H",
+    color: "#ffb512",
+    link: "https://holapittsburgh.com/",
+    video: "https://holapittsburgh.com/",
+  },
+  {
+    img: MLImagen,
+    alt: "Mlmultilingual",
+    desc: "Mlmultilingual",
+    letra: "M",
+    color: "#df3b4a",
+    link: "https://www.mlmultilingualservices.com/",
+    video: "https://www.mlmultilingualservices.com/",
+  },
 
-    {
-        img: mezcalImagen,
-        alt: "Mezcal",
-        desc: "Mezcal",
-        link: "https://monarcatradingllc.com/",
-        video: "https://monarcatradingllc.com/"
-    },
-    {
-        img: quatumImagen,
-        alt: "Quantumvibe",
-        desc: "Quantumvibe", 
-        link: "https://www.quantumvibellc.net/",
-        video: "https://www.quantumvibellc.net/"
-    },
-    {
-        img: sanconsImagen,
-        alt: "Sanconstructions",         
-        desc: "Sanconstructions",
-        link: "https://sanconstructionsllc.com/",
-        video: "https://sanconstructionsllc.com/"
-    },
-    {
-        img: tastyImagen,
-        alt: "Tasty",
-        desc: "Tasty",
-        link: "https://tastytaquitos.com/",
-        video: "https://tastytaquitos.com/"
-    }
+  {
+    img: mezcalImagen,
+    alt: "Mezcal",
+    desc: "Mezcal",
+    letra: "M",
+    color: "#a77f5a",
+    link: "https://monarcatradingllc.com/",
+    video: "https://monarcatradingllc.com/",
+  },
+  {
+    img: quatumImagen,
+    alt: "Quantumvibe",
+    desc: "Quantumvibe",
+    letra: "Q",
+    color: "#165788",
+    link: "https://www.quantumvibellc.net/",
+    video: "https://www.quantumvibellc.net/",
+  },
+  {
+    img: sanconsImagen,
+    alt: "Sanconstructions",
+    desc: "Sanconstructions",
+    letra: "S",
+    color: "#919095",
+    link: "https://sanconstructionsllc.com/",
+    video: "https://sanconstructionsllc.com/",
+  },
+  {
+    img: tastyImagen,
+    alt: "Tasty",
+    desc: "Tasty",
+    letra: "T",
+    color: "#006749",
+    link: "https://tastytaquitos.com/",
+    video: "https://tastytaquitos.com/",
+  },
+];
+
+const slidesCipax = slidesInfo.map((slide, index) => (
+  <a
+    className="slide-container-cipax"
+    title={slide.alt}
+    href={slide.video}
+    target="_blank"
+    rel="noreferrer"
+    style={{
+      backgroundColor: slide.color,
+      opacity: "0.9",
+    }}
+    key={index}
+  >
+   
+      <div className="imgCipax">
+        <span
+          style={{
+            fontSize: "60px",
+            color: "#fff",
+            fontWeight: "bold",
+          }}
+        >
+          {slide.letra}
+        </span>
+      </div>
+
+      <div className="slide-desc">
+        <span>{slide.alt}</span>
+      </div>
     
-    
-
-]
-
-const slidesCipax = slidesInfo.map((slide) => (
-    <div className="slide-container-cipax">
-        <a className="imgCipax" title={slide.alt} href={slide.video} alt={slide.alt} target="_blank" rel="noreferrer"><img title={slide.alt} src={slide.img} alt={slide.alt}/> </a>             
-        <div className="slide-desc">
-            <span>{slide.alt}</span>
-        </div>
-    </div>
-
-
+  </a>
 ));
 
 export default slidesCipax;
